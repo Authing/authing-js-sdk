ca30bb3f59d45d7e5908ac6847ee5e34da326ae7
ivy <597055914@qq.com>
2018-08-29 18:24:12 +0800

    Remove authHeaderMiddleware.js

--- AuthHeaderMiddleware.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(req, next, token) {
-    if (!req.options.headers) {
-      req.options.headers = {};
-    }
-
-    req.options.headers.authorization = token ? `Bearer ${token}` : null;
-    next();
-}
