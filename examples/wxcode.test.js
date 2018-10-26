95c0c724ff59bce02c501d01ab687e211a6140a0
leinue <597055914@qq.com>
2018-10-26 08:55:30 +0800

    release v1.1.4

---
+++ examples/wxcode.test.js
@@ -0,0 +1,15 @@
+var Authing = require('../index.js');
+
+var secret = 'b41a29583618d8e9de201d5e80db7056';
+var clientId = '5a97ede6f8635a00018551a1';
+
+var auth = new Authing({
+	clientId: clientId,
+	secret: secret
+});
+
+auth.then(function(auth) {
+	auth.startWXAppScaning({
+		redirect: true
+	});
+});
