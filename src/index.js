6cca5d9c0f992dd5e7235e4c6bab26c055165b17
Mark Yeh <yelexin@hotmail.com>
2019-07-11 20:54:03 +0800

    fix: 拼写错误

--- src/index.js
+++ src/index.js
@@ -789,7 +789,7 @@ Authing.prototype = {
 
     if (options.password) {
       if (!options.oldPassword) {
-        throw 'oldPasswordin options is not provided';
+        throw 'oldPassword in options is not provided';
       }
       options.password = encryption(options.password);
       options.oldPassword = encryption(options.oldPassword);
