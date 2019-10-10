3eb14a8d2d4211c4e75dab453c60092b93332b86
Mark Yeh <yelexin@hotmail.com>
2019-10-10 21:50:39 +0800

    refact

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,35 @@
-module.exports = {
-    entry: './index.js',
-    output: {
-        path: __dirname + '/dist',
-        filename: 'authing-js-sdk.js'
-    },
-  	module: {
-    	rules: [
-      		{
-        		test: /\.json$/,
-        		use: 'json-loader'
-      		}
-    	]
-  	}
-}
+const path = require("path");
+const webpack = require("webpack");
+const webConfig = {
+  entry: "./src/index.js",
+  target: "web",
+  output: {
+    filename: "authing-js-sdk-browser.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.BUILD_TARGET": JSON.stringify("web")
+    })
+  ]
+};
+const nodeConfig = {
+  entry: "./src/index.js",
+  target: "node",
+  output: {
+    filename: "authing-js-sdk-node.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.BUILD_TARGET": JSON.stringify("node")
+    })
+  ]
+};
+module.exports = [webConfig, nodeConfig];
