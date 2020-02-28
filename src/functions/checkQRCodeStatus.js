412f93db83cc7350345142c9a86b742b331c035e
廖长江 <changjiangthecoder@gmail.com>
2020-02-28 15:02:40 +0800

    add api: startPollingQRCodeStatus and checkQRCodeStatus

---
+++ src/functions/checkQRCodeStatus.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { convertObjectToQueryString } from "../utils/queryString"
+
+// 检验微信二维码扫描状态
+export function checkQRCodeStatus(options) {
+  if (!options) {
+    throw Error('options is not provided.');
+  }
+  if (!(typeof options === 'object' && options !== null)) {
+    throw Error('options must be an object.');
+  }
+
+  let { qrcodeId, scence } = options
+  if (!qrcodeId) {
+    throw Error('qrcodeId is not provided.');
+  }
+
+  if (scence !== "APP_AUTH") {
+    throw Error(`Unsupported scence ${scence}, the choices are APP_AUTH`)
+  }
+  let host = this.opts.host.oauth.replace("/graphql", "");
+  let queryString = convertObjectToQueryString({ qrcodeId })
+  let url = `${host}/oauth/scan-qrcode/check?${queryString}`
+
+  return axios.get(url).then(res => {
+    return res.data
+  }).catch(err => {
+    return err
+  });
+}
