90427e36fcf468815b3f89e1fe43cb589530ee8c
cj <1066983132@qq.com>
2020-03-15 14:24:05 +0800

    前端SDK不生成 random, 以便使用后端的缓存

--- src/functions/genQRCode.js
+++ src/functions/genQRCode.js
@@ -1,15 +1,15 @@
 import axios from "axios";
-import { randomWord } from "../utils/randomWord";
 export default function genQRCode(clientId) {
-  const random = randomWord(true, 12, 24);
-  if (typeof sessionStorage !== "undefined") {
-    sessionStorage.randomWord = random;
-  }
 
   let url = this.opts.host.oauth;
   url = url.replace("/graphql", "");
 
   return axios.get(
-    `${url}/oauth/wxapp/qrcode/${clientId}?random=${random}&useSelfWxapp=${this.opts.useSelfWxapp}&enableFetchPhone=${this.opts.enableFetchPhone}`
-  );
+    `${url}/oauth/wxapp/qrcode/${clientId}?useSelfWxapp=${this.opts.useSelfWxapp}&enableFetchPhone=${this.opts.enableFetchPhone}`
+  ).then(res => {
+    if (typeof sessionStorage !== "undefined") {
+      sessionStorage.randomWord = res.data.data.random;
+    }
+    return res
+  });
 }
