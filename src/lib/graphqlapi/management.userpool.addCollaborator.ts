b8ea9fde89f49d1495bd68502d874e60bee526ca
cj <1066983132@qq.com>
2020-05-24 18:32:06 +0800

    fix: addCollaborator query

--- src/lib/graphqlapi/management.userpool.addCollaborator.ts
+++ src/lib/graphqlapi/management.userpool.addCollaborator.ts
@@ -3,10 +3,20 @@ import { GraphqlClient } from '../common/GraphqlClient';
 import { ManagementTokenProvider } from '../management/ManagementTokenProvider';
 
 export const addCollaborator =  async (garpqhlClient: GraphqlClient, tokenProvider: ManagementTokenProvider, variables: any) => {
-  const query = `mutation addCollaborator($userPoolId: String!, $collaboratorUserId: String!, $permissionDescriptors: [PermissionDescriptorsInputType]!){
-    addCollaborator(userPoolId: $userPoolId, collaboratorUserId: $collaboratorUserId, permissionDescriptors: $permissionDescriptors){
-        _id
-}`
+  const query = `mutation addCollaborator(
+    $userPoolId: String!
+    $collaboratorUserId: String!
+    $permissionDescriptors: [PermissionDescriptorsInputType]!
+  ) {
+    addCollaborator(
+      userPoolId: $userPoolId
+      collaboratorUserId: $collaboratorUserId
+      permissionDescriptors: $permissionDescriptors
+    ) {
+      _id
+    }
+  }
+  `
   const token = await tokenProvider.getAccessToken()
   return await garpqhlClient.request({
     query,
