5d8e12acb16dcdb23a7818de090d90a64b9276f5
cj <cj@authing.cn>
2020-10-17 16:51:49 +0800

    feat: 用户池管理模块文档

--- src/lib/management/UserpoolManagementClient.spec.ts
+++ src/lib/management/UserpoolManagementClient.spec.ts
@@ -1,10 +1,26 @@
 import { ManagementClient } from './ManagementClient';
-import { getOptionsFromEnv } from '../testing-helper';
+import { generateRandomString, getOptionsFromEnv } from '../testing-helper';
 import test from 'ava';
+import _ from 'lodash';
 
 const management = new ManagementClient(getOptionsFromEnv());
 
 test('获取环境变量列表', async t => {
-  const data = await management.userpool.env();
+  const data = await management.userpool.listEnv();
   t.assert(data);
+});
+
+test('添加环境变量', async t => {
+  const key = generateRandomString();
+  await management.userpool.addEnv(key, generateRandomString());
+  const list = await management.userpool.listEnv();
+  t.assert(_.some(list, item => item.key === key));
+});
+
+test('删除环境变量', async t => {
+  const key = generateRandomString();
+  await management.userpool.addEnv(key, generateRandomString());
+  await management.userpool.removeEnv(key);
+  const list = await management.userpool.listEnv();
+  t.assert(!_.some(list, item => item.key === key));
 });
