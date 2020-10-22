907102e2158f1cd35830b5881f7d9c1f6a4d5cca
cj <cj@authing.cn>
2020-10-22 20:02:38 +0800

    rest request onError

--- src/lib/common/HttpClient.ts
+++ src/lib/common/HttpClient.ts
@@ -33,6 +33,7 @@ export class HttpClient {
     const { data } = await this.axios.request(config);
     const { code, message } = data;
     if (code !== 200) {
+      this.options.onError(code, message, data.data);
       throw new Error(message);
     }
     return data.data;
