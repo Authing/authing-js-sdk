1cf3420d7dafea6214dd4f390062990cfd4e52ad
cj <cj@authing.cn>
2020-10-24 18:32:47 +0800

    feat: 生成 php java 文档

--- build-doc.js
+++ build-doc.js
@@ -97,7 +97,7 @@ ${examples.map(x => x.value.split('\\`\\`\\`').join('```'))}
   fs.writeFileSync(source, docs.join('\n'))
 
   // 移动到 docs
-  const target = `../../docs/docs/sdk/sdk-for-node/${module}/${filename.replace('.ts', '')}.md`
+  const target = `../authing-docs/docs/sdk/sdk-for-node/${module}/${filename.replace('.ts', '')}.md`
   fs.copyFileSync(source, target)
 }
 
@@ -202,7 +202,7 @@ ${example ? example : ""}
   }
   const module = `${file.split('/')[file.split('/').length - 2]}`
   // 移动到 docs
-  const target = `../../docs/docs/sdk/sdk-for-python/${module}/${filename.replace('.ts', '')}.md`
+  const target = `../authing-docs/docs/sdk/sdk-for-python/${module}/${filename.replace('.ts', '')}.md`
   fs.writeFileSync(target, docs.join('\n'))
 }
 
@@ -212,11 +212,6 @@ for (let file of files) {
 
   if (file.includes('QrCodeAuthenticationClient')
     || file.includes('OrgManagementClient')
-    || file.includes('GroupsManagementClient')
-    || file.includes('PoliciesManagementClient')
-    || file.includes('UdfManagementClient')
-    || file.includes('WhitelistManagementClient')
-    || file.includes('UserpoolManagementClient')
     || file.includes('MfaAuthenticationClient')
   ) {
     continue
@@ -308,6 +303,212 @@ ${example ? example : ""}
   }
   const module = `${file.split('/')[file.split('/').length - 2]}`
   // 移动到 docs
-  const target = `../../docs/docs/sdk/sdk-for-csharp/${module}/${filename.replace('.ts', '')}.md`
+  const target = `../authing-docs/docs/sdk/sdk-for-csharp/${module}/${filename.replace('.ts', '')}.md`
+  fs.writeFileSync(target, docs.join('\n'))
+}
+
+
+// JAVA
+for (let file of files) {
+
+  if (file.includes('QrCodeAuthenticationClient')
+    || file.includes('OrgManagementClient')
+    || file.includes('GroupsManagementClient')
+    || file.includes('WhitelistManagementClient')
+    || file.includes('MfaAuthenticationClient')
+  ) {
+    continue
+  }
+
+  const data = fs.readFileSync(file, 'utf8')
+  const parsed = parseComments.parse(data);
+  let docs = []
+
+  for (const block of parsed) {
+    const { description, tags, raw } = block
+    const params = _.filter(tags, { title: 'param' }).map(x => {
+      const type = raw.match(`@param {(.*?)} .*?${x.name}.*?`) ? raw.match(`@param {(.*?)} .*?${x.name}.*?`)[1] : null
+      return {
+        name: x.name,
+        description: x.description,
+        type,
+        default: x.default
+      }
+    })
+    const returns = _.filter(tags, { title: 'returns' }).map(x => {
+      const type = raw.match(/@returns {(.*?)}.*?\n/) ? raw.match(/@returns {(.*?)}.*?\n/)[1] : null
+      return {
+        name: x.name,
+        description: x.description,
+        type
+      }
+    })[0]
+    const class_ = _.find(tags, { title: 'class' })
+    const memberof = _.find(tags, { title: 'memberof' })?.description
+    const name = _.find(tags, { title: 'name' })?.name
+
+    if (name === "checkPasswordStrength" || name === 'exists') {
+      continue
+    }
+
+    const name_zh = _.find(tags, { title: 'name_zh' })?.description
+
+
+    // 处理 nodejs
+    const args = params.map(x => x.name).filter(x => !x.includes('.')).join(', ')
+
+
+    let doc = ''
+    if (!class_) {
+      const exampleFilename = `./docs/examples/${memberof}/${name}.md`
+      console.log(exampleFilename)
+      let example = ""
+      if (fs.existsSync(exampleFilename)) {
+        const lang = 'java'
+        const reg = new RegExp('```' + lang + '\\n(([\\s\\S](?!```))*)\\n```');
+        example = reg.exec(fs.readFileSync(exampleFilename, 'utf-8'))
+        if (example) {
+          example = example[0]
+        }
+      }
+      doc = `
+## ${name_zh}
+
+${memberof}().${name}(${args})
+
+> ${description}
+
+#### 参数
+
+${params.map(param => `- \`${param.name}\` \\<${param.type}\\> ${param.description} ${param.default ? `默认值为 : \`${param.default}\`。` : ''}`).join('\n')}
+
+#### 示例
+
+${example ? example : ""}
+
+      `
+    } else {
+      doc = `
+# ${class_.description}
+
+[[toc]]
+
+> ${description.split('\\`\\`\\`').join('```')}
+
+`
+    }
+
+    docs.push(doc)
+  }
+  let filename = `${file.split('/')[file.split('/').length - 1]}`
+  if (filename === 'AuthenticationClient.ts') {
+    filename = 'README.ts'
+  }
+  const module = `${file.split('/')[file.split('/').length - 2]}`
+  // 移动到 docs
+  const target = `../authing-docs/docs/sdk/sdk-for-java/${module}/${filename.replace('.ts', '')}.md`
+  fs.writeFileSync(target, docs.join('\n'))
+}
+
+
+// PHP
+for (let file of files) {
+
+  if (file.includes('QrCodeAuthenticationClient')
+    || file.includes('OrgManagementClient')
+    || file.includes('GroupsManagementClient')
+    || file.includes('WhitelistManagementClient')
+    || file.includes('MfaAuthenticationClient')
+  ) {
+    continue
+  }
+
+  const data = fs.readFileSync(file, 'utf8')
+  const parsed = parseComments.parse(data);
+  let docs = []
+
+  for (const block of parsed) {
+    const { description, tags, raw } = block
+    const params = _.filter(tags, { title: 'param' }).map(x => {
+      const type = raw.match(`@param {(.*?)} .*?${x.name}.*?`) ? raw.match(`@param {(.*?)} .*?${x.name}.*?`)[1] : null
+      return {
+        name: x.name,
+        description: x.description,
+        type,
+        default: x.default
+      }
+    })
+    const returns = _.filter(tags, { title: 'returns' }).map(x => {
+      const type = raw.match(/@returns {(.*?)}.*?\n/) ? raw.match(/@returns {(.*?)}.*?\n/)[1] : null
+      return {
+        name: x.name,
+        description: x.description,
+        type
+      }
+    })[0]
+    const class_ = _.find(tags, { title: 'class' })
+    const memberof = _.find(tags, { title: 'memberof' })?.description
+    const name = _.find(tags, { title: 'name' })?.name
+
+    if (name === "checkPasswordStrength" || name === 'exists') {
+      continue
+    }
+
+    const name_zh = _.find(tags, { title: 'name_zh' })?.description
+
+
+    // 处理 nodejs
+    const args = params.map(x => x.name).filter(x => !x.includes('.')).join(', ')
+
+
+    let doc = ''
+    if (!class_) {
+      const exampleFilename = `./docs/examples/${memberof}/${name}.md`
+      console.log(exampleFilename)
+      let example = ""
+      if (fs.existsSync(exampleFilename)) {
+        const lang = 'php'
+        const reg = new RegExp('```' + lang + '\\n(([\\s\\S](?!```))*)\\n```');
+        example = reg.exec(fs.readFileSync(exampleFilename, 'utf-8'))
+        if (example) {
+          example = example[0]
+        }
+      }
+      doc = `
+## ${name_zh}
+
+${memberof}().${name}(${args})
+
+> ${description}
+
+#### 参数
+
+${params.map(param => `- \`${param.name}\` \\<${param.type}\\> ${param.description} ${param.default ? `默认值为 : \`${param.default}\`。` : ''}`).join('\n')}
+
+#### 示例
+
+${example ? example : ""}
+
+      `
+    } else {
+      doc = `
+# ${class_.description}
+
+[[toc]]
+
+> ${description.split('\\`\\`\\`').join('```')}
+
+`
+    }
+
+    docs.push(doc)
+  }
+  let filename = `${file.split('/')[file.split('/').length - 1]}`
+  if (filename === 'AuthenticationClient.ts') {
+    filename = 'README.ts'
+  }
+  const module = `${file.split('/')[file.split('/').length - 2]}`
+  // 移动到 docs
+  const target = `../authing-docs/docs/sdk/sdk-for-php/${module}/${filename.replace('.ts', '')}.md`
   fs.writeFileSync(target, docs.join('\n'))
 }
