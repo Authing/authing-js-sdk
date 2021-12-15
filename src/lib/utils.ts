be74d35a00ab9dad6b231930d26e6274d70e8e45
mingyard <mingyard@163.com>
2021-12-15 15:16:01 +0800

    修改key错误

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,12 +11,9 @@ export const encrypt = async (plainText: string, publicKey: string, encryption?:
       }
       try {
         const { sm2 } = require('sm-crypto')
-        console.log('模块加载成功')
-        const encrypted = sm2.doEncrypt(plainText, publicKey);
-        console.log('encrypted', encrypted)
-        return resolve(encrypted);
+        const encrypted = sm2.doEncrypt(plainText, encryption.publicKey);
+        resolve(encrypted);
       } catch (err) {
-        console.log(err)
         throw new Error('未安装模块: sm-crypto');
       }
     }
