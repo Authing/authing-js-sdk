c289185cbd269fd1ac3a45096211ceb75b31cef1
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2022-10-24 00:08:27 +0800

    test: run miniapp example for version 5.1.2 :rocket:

--- examples/miniapp-wx/miniapp/pages/index/index.ts
+++ examples/miniapp-wx/miniapp/pages/index/index.ts
@@ -8,7 +8,7 @@ import { encryptFunction } from '@authing/miniapp-sm2encrypt'
 
 const authing = new Authing({
   appId: '630ed3137dd6f2fd7001da24',
-  host: 'https://test-auth-zhaoyiming.pre.authing.cn',
+  host: 'https://test-auth-zhaoyiming.authing.cn',
   userPoolId: '62e221f85f5ac5cc47037a39',
   encryptFunction
 })
