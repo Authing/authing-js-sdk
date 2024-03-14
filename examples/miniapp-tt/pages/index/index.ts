180d8149950382897b7abfa132eebf74d15e752f
dongfengtao <dongfengtao@authing.com>
2024-03-14 11:18:04 +0800

    fix:增加 抖音 原生 sdk

--- examples/miniapp-tt/pages/index/index.ts
+++ examples/miniapp-tt/pages/index/index.ts
@@ -1,4 +1,24 @@
-const app = getApp()
+// index.ts
+import { Authing } from '@authing/miniapp-tt'
+
+// // 按需使用，加密密码
+// // import { encryptFunction } from '@authing/miniapp-jsencrypt'
+
+// import { encryptFunction } from '@authing/miniapp-sm2encrypt'
+// const AUTHING_EXT_IDP_CONN_IDENTIFIER = "douyin-mini2"
+
+// const APP_SECRET = '04093a8d20472bc1ea7d2207d69751ca99895d82';
+// const APP_ID = 'ttcad4bc6955e5062401';
+// const authing = new Authing({
+//   appId: '65e817bc434b729d058140a0',
+//   host: 'https://minnappauthing.why.lixpng.top',
+//   userPoolId: '6321a29180d12ab3a3e35306',
+//   encryptFunction
+// })
+
+// const app = getApp()
+
+// console.log(Authing,'AuthingAuthingdfft')
 
 Page({
   data: {
